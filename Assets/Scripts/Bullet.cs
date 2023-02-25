33de45c733df39e76f8e3262f38034e1cefa1169
Aiden C. Desjarlais <71170613+TOG11@users.noreply.github.com>
2023-02-25 11:38:04 -0600

    initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    internal bool stop;
+    void FixedUpdate()
+    {
+        int layerMask = 1 << 8;
+
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask) && !stop)
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Spawner.singleton.Enemys.ForEach((enemy) =>
+            {
+                if (hit.transform.gameObject.tag == "SHIP")
+                {
+                    enemy.Health.RemoveHealth(Random.Range(0, 20));
+                    if (enemy.Health.HP <= 0)
+                        Destroy(hit.transform.gameObject);
+                    else
+                        stop = true;
+                }
+            });
+        }
+    }
+
+    private void OnCollisionEnter()
+    {
+        Destroy(gameObject);
+    }
+}
